ad1654adc4644dcb050bceafcf65a99658a257dc
Jack Sell <98410625+jhdunc@users.noreply.github.com>
2023-11-12 19:31:43 +0100

    add start animation

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    public PlayerMovementRB movement;
+    public Animator anim;
+    void Start()
+    {
+        movement.moveSpeed = 0;
+        movement.sr.enabled = false;
+        Invoke("SpawnAnim", 2);
+    }
+
+    public void SpawnAnim()
+    {
+        anim.SetBool("PlayerSpawn", true);
+    }    
+
+    public void SpawnPlayer()
+    {
+        anim.SetBool("PlayerSpawn", false); 
+        movement.moveSpeed = 3;
+        movement.sr.enabled = true;
+
+    }
+    void Update()
+    {
+        Debug.Log(anim.GetBool("PlayerSpawn"));
+    }
+}
